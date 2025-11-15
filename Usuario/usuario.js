77d8d2bd1b12717c6cff95ccb7b39657951a07b8
Luana-Fernandez <lufernandezvargas@gmail.com>
2025-11-15 16:47:08 -0300

    carga de médicos desde la API

--- Usuario/usuario.js
+++ Usuario/usuario.js
@@ -1,3 +1,25 @@
+//TRAER MÉDICOS DE LA API
+async function cargarMedicos() {
+    const res = await fetch("https://6913e692f34a2ff1170d7f79.mockapi.io/api/doctor");
+    const medicos = await res.json();
+    return medicos;
+}
+
+//CARGAR MÉDICOS EN SELECT
+async function cargarSelectMedicos() {
+    const lista = await cargarMedicos();
+    const select = document.querySelector('select[name="medico"]');
+    select.innerHTML = `<option value="">Seleccione un médico</option>`;
+
+    lista.forEach(med => {
+        const opt = document.createElement("option");
+        opt.value = med.nombre;
+        opt.textContent = med.nombre;
+        select.appendChild(opt);
+    });
+}
+
+cargarSelectMedicos();
 
 //SOLICITARTURNO
 let fechaSeleccionada = null;
