7ba6fda0c87a064a0329f27110cc3cedc79ae096
ALVARO-UTN <alvaro.srr@gmail.com>
2025-11-16 16:42:02 -0300

    PerfilUsuario listo

--- Usuario/PerfilUsuario.js
+++ Usuario/PerfilUsuario.js
@@ -1,20 +1,59 @@
-async function cargarPerfil(params) {
-    
-        //     <input type="text" id="nombre" placeholder="Datos Paciente">
-        // </div>
-        // <div class="campo">
-        //     <label for="apellido">Apellido:</label>
-        //     <input type="text" id="apellido" placeholder="Datos Paciente">
-        // </div>
-        // <div class="campo">
-        //     <label for="telefono">Teléfono:</label>
-        //     <input type="text" id="telefono" placeholder="Datos Paciente">
-        // </div>
-        // <div class="campo">
-        //     <label for="email">Email:</label>
-        //     <input type="email" id="email" placeholder="Datos Paciente">
-        // </div>
-        // <div class="botones"></div>
+async function cargarPerfil() {
+    const datoUsuario = JSON.parse(localStorage.getItem("lsUsuario"));
+    console.log(datoUsuario);
+    const nombre = document.getElementById('nombre');
+    nombre.value = datoUsuario.nombre;
+    const apellido = document.getElementById('apellido');
+    apellido.value = datoUsuario.apellido;
+    const telefono = document.getElementById('telefono');
+    telefono.value = datoUsuario.telefono;
+    const email = document.getElementById('email');
+    email.value = datoUsuario.email;
+    const password = document.getElementById('password');
+    password.value = datoUsuario.password;
+    let estadoInput = document.getElementsByClassName('datoPerfil');
+    for (let e of estadoInput) {
+        e.disabled = true;
+    }
 }
 
+function modificarEstado() {
+    let estadoInput = document.getElementsByClassName('datoPerfil');
+    for (let e of estadoInput) {
+        e.disabled = false;
+    }
+    let btnActualizar = document.getElementById('guardar');
+    btnActualizar.disabled = false;
+}
+async function actualizarDatos() {
+    let datos = JSON.parse(localStorage.getItem('lsUsuario'));
+    let id = datos.id;
+    const datosNuevos = {
+        nombre: document.getElementById('nombre').value,
+        apellido: document.getElementById('apellido').value,
+        telefono: document.getElementById('telefono').value,
+        password: document.getElementById('password').value,
+    }
+
+    alert(id)
+    const respuesta = await fetch(`https://6913e692f34a2ff1170d7f79.mockapi.io/api/users/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(datosNuevos),
+    });
+    if (!respuesta.ok) {
+        alert('Falla al Actualizar')
+    }
+    alert('Actualizado Correctamente, relogee');
+    window.location.href = "/main.html";
+
+}
+
+const btnModificar = document.getElementById('modificar');
+btnModificar.addEventListener('click', modificarEstado)
+
+const btnGuardar = document.getElementById('guardar');
+btnGuardar.addEventListener('click', actualizarDatos);
 cargarPerfil();
