8193897b2858df89779c42d2d248a7822f95122d
ALVARO-UTN <alvaro.srr@gmail.com>
2025-11-17 11:24:33 -0300

    fecha Turno incompleto

--- Usuario/usuario.js
+++ Usuario/usuario.js
@@ -1,18 +1,4 @@
 
-    // TERMINAR DE HACER LOS LA CARGA DE OPCIONES CON CONDICIONES//
-//Cargar Especialidad
-async function cargarEspecialidad() {
-    let listaEspecialidad = document.getElementById('especialidad');
-    let datoEspecialidad = await cargarMedicos();
-    for (let i of datoEspecialidad) {
-        let opcion = document.createElement('option');
-        opcion.innerHTML = `
-    <option value="" >${i.especialidad}</option>
-    `;
-        listaEspecialidad.append(opcion);
-    }
-}
-cargarEspecialidad();
 
 //TRAER MÉDICOS DE LA API
 async function cargarMedicos() {
@@ -21,21 +7,49 @@ async function cargarMedicos() {
     return medicos;
 }
 
-//CARGAR MÉDICOS EN SELECT
+//Cargar Especialidad
+
+async function cargarEspecialidad() {
+    let listaEspecialidad = document.getElementById('especialidad');
+    let datoEspecialidad = await cargarMedicos();
+
+    // Usamos un Set para evitar duplicados
+    let especialidadesUnicas = new Set();
+
+    for (let i of datoEspecialidad) {
+        if (!especialidadesUnicas.has(i.especialidad)) {
+            especialidadesUnicas.add(i.especialidad);
+
+            let opcion = document.createElement('option');
+            opcion.value = i.especialidad;
+            opcion.textContent = i.especialidad;
+            listaEspecialidad.append(opcion);
+        }
+    }
+}
+
+
+
+
+//CARGAR MÉDICOS en base a la especialidad
 async function cargarSelectMedicos() {
     const lista = await cargarMedicos();
+    let optEspecialidad = document.getElementById('especialidad').value;
     const select = document.querySelector('select[name="medico"]'); // agrege name en el html, solo tenia id
     select.innerHTML = `<option value="">Seleccione un médico</option>`;
 
     lista.forEach(med => {
-        const opt = document.createElement("option");
-        opt.value = med.nombre;
-        opt.textContent = med.nombre;
-        select.appendChild(opt);
+        if (med.especialidad == optEspecialidad) {
+            const opt = document.createElement("option");
+            opt.value = med.nombre;
+            opt.textContent = med.nombre;
+            select.appendChild(opt);
+        }
+
     });
 }
 
-cargarSelectMedicos();
+// carga los dias en base al medico
 async function cargarDias() {
     let diasMedico = document.getElementById('medico').value;
     let lista = document.getElementById('dia');
@@ -43,11 +57,10 @@ async function cargarDias() {
     let datos = await cargarMedicos();
     for (let n of datos) {
         if (n.nombre == diasMedico) {
-            alert('encontrado')
             for (let d of n.diasDisponibles) {
-                let optDia = document.createElement('option')
+                let optDia = document.createElement('li')
                 optDia.innerHTML = `
-                <option value="" >${d.dia}</option>
+                <li >${d.dia}</li>
                 `;
                 lista.append(optDia);
             }
@@ -56,8 +69,72 @@ async function cargarDias() {
     }
 }
 
+// fecha minima para elegir
+const fechaInput = document.getElementById('fechaTurno');
+const hoy = new Date().toISOString().split('T')[0];
+fechaInput.min = hoy;
+//
+
+let optEspecialidad = document.getElementById('especialidad');
+optEspecialidad.addEventListener('change', cargarSelectMedicos);
 let optMedico = document.getElementById('medico');
 optMedico.addEventListener('change', cargarDias);
+
+cargarEspecialidad();
+
+// eleccion dia
+
+async function controlarDia(){
+    let fecha = document.getElementById('fechaTurno').value;
+    let diaFecha = await diaSemana(fecha);
+    let medico = await cargarMedicos();
+    let selMedico = document.getElementById('medico').value;
+    for(let m of medico){
+        if (m.nombre == selMedico){
+            //controlar esto
+            alert(m.diasDisponibles.dia.value)
+            alert(diaFecha.value)
+            if(!m.diasDisponibles.dia.includes(diaFecha)){
+                return alert('no Coincide')
+            }
+        }
+    }
+}
+
+async function diaSemana(f) {
+    const dias = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+    return dias[new Date(f+"T00:00:00").getDay];
+    
+}
+
+let fechaTurno = document.getElementById('fechaTurno');
+fechaTurno.addEventListener('change',controlarDia);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //SOLICITARTURNO
 // let fechaSeleccionada = null;
@@ -115,4 +192,4 @@ optMedico.addEventListener('change', cargarDias);
 
 
 
-//SOLICITARTURNO
+//SOL
